0590980f82a9813959d0f15a5284f0a53f4876f3
vam <vamshop@gmail.com>
2019-11-14 13:50:13 +0300

    Fixes

--- images/0.34.0/config/admin.js
+++
@@ -1,7 +0,0 @@
-// config used by dashboard client side only
-module.exports = {
-  // dashboard UI language
-  language: 'en',
-  apiBaseUrl: '/api/v1',
-  developerMode: true
-}
--- images/0.34.0/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-nginx
-exec pm2 start process.json --no-daemon
